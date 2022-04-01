ee64f435a5fd398842f73d1b0119bbf097c73014
Hyy2001X <1804430051@qq.com>
2022-04-01 15:31:07 +0800

    Fix

--- Scripts/Generate.sh
+++ Scripts/Generate.sh
@@ -10,7 +10,7 @@ do
 	TARGET=${TARGET/OTA_/}
 	gzip -9 -c ${TARGET} > OTA/${TARGET}.gz
 
-	MD5=$(md5sum OTA/${TARGET}.gz | awk '{print $1} | cut -c1-5')
+	MD5=$(md5sum OTA/${TARGET}.gz | awk '{print $1}' | cut -c1-5)
 	OTA_PKG=${TARGET}-${OTA_VERSION}-${MD5}.gz
 
 	mv -f OTA/${TARGET}.gz OTA/${OTA_PKG}
