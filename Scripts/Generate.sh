be4469e052bd8c5c7f60529471bf7cad9a6c97cb
Hyy2001X <1804430051@qq.com>
2022-04-01 15:29:30 +0800

    Tests

--- Scripts/Generate.sh
+++ Scripts/Generate.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-
 WORK="${GITHUB_WORKSPACE}"
+OTA_VERSION=$(date +%Y%m%d%H%M%S)
 
-mkdir -p output
+mkdir -p OTA
 
 for TARGET in $(ls -1 | grep 'OTA_')
 do
-	echo ${TARGET}
+	TARGET=${TARGET/OTA_/}
+	gzip -9 -c ${TARGET} > OTA/${TARGET}.gz
+
+	MD5=$(md5sum OTA/${TARGET}.gz | awk '{print $1} | cut -c1-5')
+	OTA_PKG=${TARGET}-${OTA_VERSION}-${MD5}.gz
+
+	mv -f OTA/${TARGET}.gz OTA/${OTA_PKG}
 done
