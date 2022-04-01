8b986153a8bff015890a8cd95d81b7ecc92bb8c9
Hyy2001X <1804430051@qq.com>
2022-04-01 16:03:58 +0800

    Update Generate.sh

--- Scripts/Generate.sh
+++ Scripts/Generate.sh
@@ -5,10 +5,6 @@ OTA_VERSION=$(date +%Y%m%d%H%M%S)
 
 mkdir -p OTA
 
-chown root:root -R OTA*
-chmod 777 -R OTA*
-sudo du -a
-
 for TARGET_PATH in $(ls -1 | grep 'OTA_')
 do
 	TARGET=${TARGET_PATH/OTA_/}
@@ -16,6 +12,10 @@ do
 
 	MD5=$(md5sum OTA/${TARGET}.gz | awk '{print $1}' | cut -c1-5)
 	OTA_PKG=OTA-${TARGET}-${OTA_VERSION}-${MD5}.gz
-
-	mv -f OTA/${TARGET}.gz OTA/${OTA_PKG}
+	if [[ ! $(sudo ls -1 OTA-OLD) =~ ${MD5} ]]
+	then
+		mv -f OTA/${TARGET}.gz OTA/${OTA_PKG}
+	else
+		rm -f OTA/${TARGET}.gz
+	fi
 done
