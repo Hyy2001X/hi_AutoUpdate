07b9fd37f5ed5f6bfa1f569aea4a65334c290a8e
Hyy2001X <1804430051@qq.com>
2022-04-01 16:44:02 +0800

    Fix

--- Scripts/Generate.sh
+++ Scripts/Generate.sh
@@ -4,7 +4,7 @@ WORK="${GITHUB_WORKSPACE}"
 OTA_VERSION=$(date +%Y%m%d%H%M%S)
 
 mkdir -p OTA
-sudo du ./ -a
+
 for TARGET_PATH in $(ls -1 | grep 'OTA_')
 do
 	TARGET=${TARGET_PATH/OTA_/}
@@ -12,6 +12,8 @@ do
 
 	MD5=$(md5sum OTA/${TARGET}.gz | awk '{print $1}' | cut -c1-5)
 	OTA_PKG=OTA-${TARGET}-${OTA_VERSION}-${MD5}.gz
+	
+	mv -f OTA/${TARGET}.gz OTA/${OTA_PKG}
 	#if [[ ! $(sudo ls -1 OTA-OLD) =~ ${MD5} ]]
 	#then
 	#	mv -f OTA/${TARGET}.gz OTA/${OTA_PKG}
