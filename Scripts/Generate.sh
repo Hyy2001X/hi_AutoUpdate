686aadfd29bd6be5ed5d0b0a18ee890350d22586
Hyy2001X <1804430051@qq.com>
2022-04-01 15:32:42 +0800

    Fix

--- Scripts/Generate.sh
+++ Scripts/Generate.sh
@@ -5,10 +5,10 @@ OTA_VERSION=$(date +%Y%m%d%H%M%S)
 
 mkdir -p OTA
 
-for TARGET in $(ls -1 | grep 'OTA_')
+for TARGET_PATH in $(ls -1 | grep 'OTA_')
 do
-	TARGET=${TARGET/OTA_/}
-	gzip -9 -c ${TARGET} > OTA/${TARGET}.gz
+	TARGET=${TARGET_PATH/OTA_/}
+	gzip -9 -c ${TARGET_PATH} > OTA/${TARGET}.gz
 
 	MD5=$(md5sum OTA/${TARGET}.gz | awk '{print $1}' | cut -c1-5)
 	OTA_PKG=${TARGET}-${OTA_VERSION}-${MD5}.gz
